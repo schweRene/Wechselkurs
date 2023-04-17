efdd069974b68d533983ae30d7ff0434df58a94c
Rene Schwemer <Schwemer.R@it.bbwi>
2023-04-17 12:29:08 +0200

    Initial commit

---
+++ src/main/java/com/example/Wechselkurs/WechselkursApplication.java
@@ -0,0 +1,13 @@
+package com.example.Wechselkurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechselkursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechselkursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Wechselkurs/WechselkursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Wechselkurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechselkursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
